03f61ae306c53151aa2804dadd814d052862c0a6
Jessica Hunt <jessicamarilynhunt@gmail.com>
2019-06-26 18:30:20 -0400

    add psuedocode in javascript file. change api url to limit 10 and rating pg-13.

--- gif.js
+++ gif.js
@@ -2,11 +2,32 @@ $(document).ready(function() {
     // API Key for GIPHY
     // Api Key: 7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf
     // GIPHY request URL
-    // https://api.giphy.com/v1/gifs/search?api_key=7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf&q=&limit=25&offset=0&rating=G&lang=en
     
+    //https://api.giphy.com/v1/gifs/search?api_key=7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf&q=&limit=10&offset=0&rating=PG-13&lang=en
     
 //save APIkey to variable
 //save GIPHY request URL to a variable
+
+//make a list of 10 initial categories for gifs
+//create buttons for each list item
+//display buttons at the top of the page
+
+//when button is clicked
+//display 10 gifs from the GIPHY API according to the category of that button
+//display rating for each gif
+//gifs should be paused once they are loaded to the page
+//gifs should play only when their image is clicked
+//gifs should paused if clicked again.
+
+//when a different button is clicked
+// clear the gifs from the last button click
+//replace with 10 gifs related to the current button click
+
+//when user submits a search
+//an additional button is created
+//button should display at the end of the list with a name equal to the text of their search. 
+//when the user clicks on the new button
+//the button should display 10 gifs related to their search
 
 
 });
