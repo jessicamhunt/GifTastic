69a5e110fcef55ab930996bebcf3028f315075a7
Jessica Hunt <jessicamarilynhunt@gmail.com>
2019-06-26 17:43:28 -0400

    add bootstrap form with label, text input, and submit button.

--- gif.js
+++ gif.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     // API Key for GIPHY
     // Api Key: 7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf
+    // GIPHY request URL
+    // https://api.giphy.com/v1/gifs/search?api_key=7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf&q=&limit=25&offset=0&rating=G&lang=en
+    
+    
+//save APIkey to variable
+//save GIPHY request URL to a variable
+
+
 });
