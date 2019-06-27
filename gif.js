06a52853945a090307ea9a42e40c8ec451d8bb35
Jessica Hunt <jessicamarilynhunt@gmail.com>
2019-06-27 03:23:47 -0400

    add ids to html inputs in form elements. add variable for results. add for loop for results. change gif and rating variables to target api properties and values. set source attribute for gif images. add comments for all lines of code.

--- gif.js
+++ gif.js
@@ -15,11 +15,15 @@ var apiKey = '7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf';
 var topics = ['south park', 'parks and recreation','the office', 'arrested development', 
 'adventure time', 'rick and morty','bojack horseman','the simpsons','30 rock'];
 
+
+//---------------------------------------FUNCTIONS--------------------------------------------------
+
 //function to display gifs and ratings
 function displayGif() {
-
+    
+    //variable to create attribute with show name as the value for each button
     var tvShow = $(this).attr("show-name");
-    console.log("tv show" + tvShow);
+    console.log(tvShow);
     
     //save GIPHY request URL to a variable
     var queryUrl = 
@@ -31,55 +35,86 @@ function displayGif() {
         method: "GET"
     }).then(function(response) {
         console.log('response' + response);
-        
-        //div to hold individual gif and rating
-        var gifContent = $('<div class="gif-content">');
-        console.log('gif content' + gifContent);
+        //variable to hold results
+        var results = response.data;
+        console.log(results);
 
-        //variable to hold gif
-        var gif = response;
-        console.log(response);
+        for(var i = 0; i < results.length; i++) {
 
-        // variable to show gif
-        var gifDisplay = $('<img>').attr("src", gif);
-        console.log(gifDisplay);
-
-        //append gif to gifContent div
-        gifContent.append(gifDisplay);
-
-        //variable to hold gif rating
-        var rating = response.rating;
-        console.log('rating' + rating);
-
-        //variable to display gif rating
-        var ratingDisplay = $('<p>').text("Rated: " + rating);
-        console.log('rating display' + ratingDisplay);
-
-        //append rating display to gifContent div
-        gifContent.append(ratingDisplay)
-
-        //display gifContent div in html
-        $('#gif-display').prepend(gifContent);
-        
+            //div to hold gif and rating content
+            var gifContent = $('<div class="gif-content">');
+            console.log('gif content' + gifContent);
+            
+            //variable to hold gif
+            var gif = results[i].images.original.url;
+            console.log(gif);
+            
+            // variable to show gif
+            var gifDisplay = $('<img>')
+            console.log(gifDisplay);
+            
+            gifDisplay.attr("src", gif);
+            gifDisplay.attr("alt", "tv gif")
+            //append gif to gifContent div
+            gifContent.append(gifDisplay);
+            
+            //variable to hold gif rating
+            var rating = results[i].rating;
+            console.log('rating' + rating);
+            
+            //variable to display gif rating
+            var ratingDisplay = $('<p>').text("Rated: " + rating);
+            console.log('rating display' + ratingDisplay);
+            
+            //append rating display to gifContent div
+            gifContent.append(ratingDisplay)
+            
+            //display gifContent div in html
+            $('#gif-display').prepend(gifContent);    
+        }
     });
 }    
-console.log(displayGif());
-displayGif();
+// console.log(displayGif());
+// displayGif();
 
 
 //create buttons for each list item
 function createButton(){
+    
+    $("#display-buttons").empty();
+    
     for(var i = 0; i < topics.length; i++) {
-    //   $("#display-buttons").empty();
-      var buttonName = $("<button>");
-      buttonName.addClass("show");
-      buttonName.attr("show-name", topics[i]);  
-      //display buttons at the top of the page
-      buttonName.text(topics[i]);
-      $("#display-buttons").append(buttonName);
+      
+        var buttonName = $("<button>");
+      
+        buttonName.addClass("show");
+      
+        buttonName.attr("show-name", topics[i]);  
+      
+        //display buttons at the top of the page
+      
+        buttonName.text(topics[i]);
+     
+        $("#display-buttons").append(buttonName);
     };
 };
-//when button is clicked
+
+//click event functions
+$("#add-show").on("click", function(e) {
+    e.preventDefault();
+    // This line grabs the input from the textbox
+    var show = $("#input-show").val().trim();
+
+    // Adding show from the textbox to our array
+    topics.push(show);
+
+    // Calling renderButtons which handles the processing of our movie array
+    createButton();
+});
+
+ // add click function for buttons on page
+$(document).on("click", ".show", displayGif);
+
 //display 10 gifs from the GIPHY API according to the category of that button
 //display rating for each gif
 //gifs should be paused once they are loaded to the page
