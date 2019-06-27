3c765106c2664dad22cb8efab90c3128f9a39b9c
Jessica Hunt <jessicamarilynhunt@gmail.com>
2019-06-27 00:36:14 -0400

    add divs and variables to gif display function to store and display gif image and ratings. display this data in html.

--- gif.js
+++ gif.js
@@ -4,22 +4,71 @@ $(document).ready(function() {
 
 // API Key for GIPHY
 // Api Key: 7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf
-var apiKey = '7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf';
-var tvShow = '';
+
 // GIPHY request URL
-//https://api.giphy.com/v1/gifs/search?api_key=7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf&q=&limit=10&offset=0&rating=PG-13&lang=en
-var queryUrl = 
-'https://api.giphy.com/v1/gifs/search?q=cheeseburgers&api_key='+ apiKey + '&limit=10&offset=0&rating=PG-13&lang=en';
+//https://api.giphy.com/v1/gifs/search?q=(search-term)&api_key=7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf&limit=10&offset=0&rating=PG-13&lang=en
 
 //save APIkey to variable
-//save GIPHY request URL to a variable
+var apiKey = '7vUfrLQpKYQim7mTbbeHRt8iuv9URaZf';
 
 //make a list of 10 initial categories for gifs
 var topics = ['south park', 'parks and recreation','the office', 'arrested development', 
-              'adventure time', 'rick and morty','bojack horseman','the simpsons','30 rock'];
+'adventure time', 'rick and morty','bojack horseman','the simpsons','30 rock'];
+
+//function to display gifs and ratings
+function displayGif() {
+
+    var tvShow = $(this).attr("show-name");
+    console.log("tv show" + tvShow);
+    
+    //save GIPHY request URL to a variable
+    var queryUrl = 
+    'https://api.giphy.com/v1/gifs/search?q=' + tvShow + '&api_key=' + apiKey + '&limit=10&offset=0&rating=PG-13&lang=en';
+    
+    //connect to GIPHY api
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response) {
+        console.log('response' + response);
+        
+        //div to hold individual gif and rating
+        var gifContent = $('<div class="gif-content">');
+        console.log('gif content' + gifContent);
+
+        //variable to hold gif
+        var gif = response;
+        console.log(response);
+
+        // variable to show gif
+        var gifDisplay = $('<img>').attr("src", gif);
+        console.log(gifDisplay);
+
+        //append gif to gifContent div
+        gifContent.append(gifDisplay);
+
+        //variable to hold gif rating
+        var rating = response.rating;
+        console.log('rating' + rating);
+
+        //variable to display gif rating
+        var ratingDisplay = $('<p>').text("Rated: " + rating);
+        console.log('rating display' + ratingDisplay);
+
+        //append rating display to gifContent div
+        gifContent.append(ratingDisplay)
+
+        //display gifContent div in html
+        $('#gif-display').prepend(gifContent);
+        
+    });
+}    
+console.log(displayGif());
+displayGif();
+
+
 //create buttons for each list item
 function createButton(){
-
     for(var i = 0; i < topics.length; i++) {
     //   $("#display-buttons").empty();
       var buttonName = $("<button>");
@@ -30,14 +79,7 @@ function createButton(){
       $("#display-buttons").append(buttonName);
     };
 };
-
 //when button is clicked
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
 //display 10 gifs from the GIPHY API according to the category of that button
 //display rating for each gif
 //gifs should be paused once they are loaded to the page
